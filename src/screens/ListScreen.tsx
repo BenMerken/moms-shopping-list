1e8f3634cbc624fd8d78fb698361973051c0f792
Ben Merken <ben.merken@gmail.com>
2023-03-19 23:22:32 +0100

    Have grocery list screen working

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,8 +1,24 @@
-import {useAsyncStorage} from '@react-native-async-storage/async-storage'
+import {FontAwesome} from '@expo/vector-icons'
+import AsyncStorage, {
+	useAsyncStorage
+} from '@react-native-async-storage/async-storage'
+import {useHeaderHeight} from '@react-navigation/elements'
 import {useEffect, useState} from 'react'
-import {FlatList, StyleSheet, Text, View} from 'react-native'
+import {
+	Alert,
+	Button,
+	Dimensions,
+	FlatList,
+	KeyboardAvoidingView,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View
+} from 'react-native'
 
-import {SafeAreaContainer} from '@components/index'
+import {CustomTextInput, SafeAreaContainer} from '@components/index'
+import text from '@utils/text'
+import theme from '@utils/theme'
 
 type ListItemProps = {
 	item: GroceryListItem
@@ -10,23 +26,88 @@ type ListItemProps = {
 
 const styles = StyleSheet.create({
 	groceryListItems: {
-		
+		alignItems: 'center'
+	},
+	noItemsPlaceholder: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	groceryListItem: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		alignItems: 'center',
+		gap: 32,
+		marginBottom: 8,
+		padding: 16,
+		width: Dimensions.get('screen').width * 0.8,
+		backgroundColor: 'white',
+		...theme.dropShadow
+	},
+	newItemForm: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginBottom: 32
+	},
+	input: {
+		marginBottom: 16
 	}
 })
 
 const ListScreen = ({route}: StackScreenProps<'List'>) => {
-	const [groceryList, setGroceryList] = useState<GroceryList | undefined>(
-		undefined
-	)
+	const [groceryList, setGroceryList] = useState<GroceryList>()
+	const [newItemName, setNewItemName] = useState('')
+
+	const headerHeight = useHeaderHeight()
 
 	const {getItem} = useAsyncStorage(route.params.listUuid)
 
 	const _listItem = ({item}: ListItemProps) => {
+		const removeItem = async () => {
+			const newGroceryList = {
+				...groceryList!,
+				items: groceryList!.items!.filter(
+					(itemOnList) => itemOnList !== item
+				)
+			}
+
+			await AsyncStorage.mergeItem(
+				groceryList!.uuid,
+				JSON.stringify(newGroceryList)
+			)
+			setGroceryList(newGroceryList)
+		}
+
 		return (
-			<View>
+			<View key={item} style={styles.groceryListItem}>
+				<TouchableOpacity onPress={removeItem}>
+					<FontAwesome name='close' size={24} />
+				</TouchableOpacity>
 				<Text>{item}</Text>
 			</View>
 		)
+	}
+
+	const addItem = async () => {
+		if (groceryList!.items!.includes(newItemName)) {
+			Alert.alert(
+				`Er bestaat al een artikel met naam "${newItemName}" in dit lijstje`
+			)
+
+			return
+		}
+
+		const newGroceryList = {
+			...groceryList!,
+			items: [...groceryList!.items!, newItemName]
+		}
+
+		await AsyncStorage.mergeItem(
+			groceryList!.uuid,
+			JSON.stringify(newGroceryList)
+		)
+		setGroceryList(newGroceryList)
+		setNewItemName('')
 	}
 
 	useEffect(() => {
@@ -41,8 +122,37 @@ const ListScreen = ({route}: StackScreenProps<'List'>) => {
 
 	return (
 		<SafeAreaContainer>
-			<Text>{groceryList?.name}</Text>
-			<FlatList style={styles.groceryListItems} data={groceryList?.items} renderItem={_listItem} />
+			{!groceryList?.items?.length ? (
+				<View style={styles.noItemsPlaceholder}>
+					<Text>Dit lijstje is (nog) leeg...</Text>
+				</View>
+			) : (
+				<FlatList
+					contentContainerStyle={styles.groceryListItems}
+					data={groceryList?.items}
+					renderItem={_listItem}
+				/>
+			)}
+			<KeyboardAvoidingView
+				style={styles.newItemForm}
+				keyboardVerticalOffset={headerHeight}
+				behavior='padding'
+			>
+				<Text style={{...text.subtitle}}>Nieuw Artikel</Text>
+				<CustomTextInput
+					inputGroupStyle={styles.input}
+					label='Naam artikel'
+					value={newItemName}
+					placeholder='bijv. "Boter/Kaas/eieren..."'
+					onChangeText={(newName) => setNewItemName(newName)}
+				/>
+				<Button
+					title='+ Artikel toevoegen'
+					disabled={!newItemName}
+					color={theme.light.primary}
+					onPress={addItem}
+				/>
+			</KeyboardAvoidingView>
 		</SafeAreaContainer>
 	)
 }
