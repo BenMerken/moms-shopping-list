8a713830356bb91976f5d20c2530c5b787b286a3
Ben Merken <ben.merken@gmail.com>
2023-04-01 18:04:28 +0200

    Remove unnecessary colorScheme property from Navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import {registerRootComponent} from 'expo'
 import {SafeAreaProvider} from 'react-native-safe-area-context'
 
 import useCachedResources from './hooks/useCachedResources'
-import useColorScheme from './hooks/useColorScheme'
 import Navigation from './navigation'
 
 const App = () => {
 	const isLoadingComplete = useCachedResources()
-	const colorScheme = useColorScheme()
 
 	if (!isLoadingComplete) {
 		return null
 	} else {
 		return (
 			<SafeAreaProvider>
-				<Navigation colorScheme={colorScheme} />
+				<Navigation />
 			</SafeAreaProvider>
 		)
 	}
