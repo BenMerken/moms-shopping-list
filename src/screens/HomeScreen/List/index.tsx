ac36aa355bb45e4bad773171b243629a9987d86e
Ben Merken <ben.merken@gmail.com>
2023-11-08 12:48:24 +0100

    Fix issue, where on list views with more items than can be captured on screen at once, editing an item would be impossible, because the keyboard would close again automatically after tapping the edit icon.

--- src/screens/HomeScreen/List/index.tsx
+++ src/screens/HomeScreen/List/index.tsx
@@ -193,6 +193,7 @@ const ShoppingListsList = ({
 		<FlatList
 			// This prop is necessary to be able to tap the save icon for the list item input,
 			// since tapping outside the input ignores other click events by default.
+			removeClippedSubviews={false}
 			keyboardShouldPersistTaps='always'
 			contentContainerStyle={styles.shoppingLists}
 			data={shoppingLists}
--- src/screens/ListScreen/List/index.tsx
+++ src/screens/ListScreen/List/index.tsx
@@ -182,6 +182,7 @@ const List = ({shoppingList, setShoppingList}: ListProps) => {
 		<FlatList
 			// This prop is necessary to be able to tap the save icon for the list item input,
 			// since tapping outside the input ignores other click events by default.
+			removeClippedSubviews={false}
 			keyboardShouldPersistTaps='always'
 			contentContainerStyle={styles.shoppingListItems}
 			data={shoppingList?.items}
