343a7bc8f5ed70857dac1ae785fa87f0508f4d93
Ben Merken <ben.merken@gmail.com>
2024-10-27 19:40:41 +0100

    Add list height to correct property, and add absolute positioning to items

--- src/screens/ListScreen/List/index.tsx
+++ src/screens/ListScreen/List/index.tsx
@@ -58,6 +58,7 @@ const styles = StyleSheet.create({
 	shoppingListItem: {
 		...theme.dropShadow,
 		flexDirection: 'row',
+		position: 'absolute',
 		flexWrap: 'wrap',
 		justifyContent: 'space-between',
 		alignItems: 'center',
@@ -448,11 +449,13 @@ const List = ({shoppingList, setShoppingList}: ListProps) => {
 			// This prop is necessary to be able to tap the save icon for the list item input,
 			// since tapping outside the input ignores other click events by default.
 			removeClippedSubviews={false}
-			style={{
-				height: shoppingList.items.length * ITEM_HEIGHT
-			}}
 			keyboardShouldPersistTaps='always'
-			contentContainerStyle={styles.shoppingListItems}
+			contentContainerStyle={[
+				styles.shoppingListItems,
+				{
+					height: shoppingList.items.length * ITEM_HEIGHT
+				}
+			]}
 			data={shoppingList?.items.sort((item) => item.order)}
 			keyExtractor={(item) => item.uuid}
 			renderItem={(item) => (
