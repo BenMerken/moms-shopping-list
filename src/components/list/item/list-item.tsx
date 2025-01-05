44cd4511ee0b2b8d99eeaedcfed9e9ac64cc15a2
Ben Merken <ben.merken@gmail.com>
2025-01-05 18:22:40 +0100

    Add dragging functionality

--- src/components/list/item/list-item.tsx
+++ src/components/list/item/list-item.tsx
@@ -5,6 +5,7 @@ import Animated, {
 	interpolate,
 	interpolateColor,
 	SharedValue,
+	useAnimatedReaction,
 	useAnimatedStyle,
 	useDerivedValue,
 	useSharedValue,
@@ -18,37 +19,70 @@ import theme from '@utils/theme'
 import {ListItemPositions} from '../list'
 import {Gesture, GestureDetector} from 'react-native-gesture-handler'
 
+type NullableNumber = number | null
+
 type ListItemProps = PropsWithChildren<{
 	item: ListRenderItemInfo<any>
 	currentItemPositions: SharedValue<ListItemPositions>
 	isDragging: SharedValue<0 | 1>
 	draggingItemId: SharedValue<number>
+	maxTop: number
 }>
 
-export const LIST_ITEM_HEIGHT = 64
+export const LIST_ITEM_HEIGHT = 72
+export const LIST_ITEM_HEIGHT_WITH_MARGIN = LIST_ITEM_HEIGHT + 24
+const MIN_TOP = 0
+
+const getKeyOfValue = (
+	value: NullableNumber,
+	object: ListItemPositions
+): NullableNumber => {
+	'worklet'
+	for (const [key, val] of Object.entries(object)) {
+		if (val.updatedIndex === value) {
+			return parseInt(key)
+		}
+	}
+
+	return null
+}
 
 const ListItem = ({
 	children,
 	item,
+	currentItemPositions,
 	isDragging,
-	draggingItemId
+	draggingItemId,
+	maxTop
 }: ListItemProps) => {
 	const top = useSharedValue(0)
+	const newIndex = useSharedValue<NullableNumber>(null)
+	const currentIndex = useSharedValue<NullableNumber>(null)
+	const currentItemPositionsDerived = useDerivedValue(() => {
+		return currentItemPositions.value
+	})
+	const isDraggingDerived = useDerivedValue(() => {
+		return isDragging.value
+	})
+	const draggingItemIdDerived = useDerivedValue(() => {
+		return draggingItemId.value
+	})
 	const isCurrentDraggingItem = useDerivedValue(() => {
-		return isDragging.value === 1 && draggingItemId.value === item.index
+		return draggingItemId.value
 	})
 
 	const {colors} = useTheme()
 
 	const styles = StyleSheet.create({
-		item: {
+		itemContainer: {
+			position: 'absolute',
 			flexDirection: 'row',
 			flexWrap: 'wrap',
 			justifyContent: 'space-between',
 			alignItems: 'center',
-			marginBottom: 8,
+			height: LIST_ITEM_HEIGHT,
 			padding: 16,
-			width: layout.window.widthWithMargin,
+			width: '100%',
 			backgroundColor: colors.card
 		}
 	})
@@ -59,48 +93,149 @@ const ListItem = ({
 			transform: [
 				{
 					scale: isCurrentDraggingItem.value
-						? interpolate(isDragging.value, [0, 1], [1, 1.025])
-						: interpolate(isDragging.value, [0, 1], [1, 0.98])
+						? interpolate(
+								isDraggingDerived.value,
+								[0, 1],
+								[1, 1.025]
+						  )
+						: interpolate(
+								isDraggingDerived.value,
+								[0, 1],
+								[1, 0.98]
+						  )
 				}
 			],
 			shadowColor: isCurrentDraggingItem.value
 				? interpolateColor(
-						isDragging.value,
+						isDraggingDerived.value,
 						[0, 1],
-						[colors.primary, colors.text]
+						[colors.text, colors.primary]
 				  )
-				: undefined,
+				: colors.text,
 			shadowOffset: {
 				width: theme.dropShadow.shadowOffset.width,
 				height: isCurrentDraggingItem.value
-					? interpolate(isDragging.value, [0, 1], [0, 7])
+					? interpolate(isDraggingDerived.value, [0, 1], [0, 7])
 					: theme.dropShadow.shadowOffset.height
 			},
 			shadowOpacity: isCurrentDraggingItem.value
-				? interpolate(isDragging.value, [0, 1], [0, 0.2])
+				? interpolate(isDraggingDerived.value, [0, 1], [0, 0.2])
 				: theme.dropShadow.shadowOpacity,
 			shadowRadius: isCurrentDraggingItem.value
-				? interpolate(isDragging.value, [0, 1], [0, 10])
+				? interpolate(isDraggingDerived.value, [0, 1], [0, 10])
 				: theme.dropShadow.shadowRadius,
 			elevation: isCurrentDraggingItem.value
-				? interpolate(isDragging.value, [0, 1], [0, 5])
+				? interpolate(isDraggingDerived.value, [0, 1], [0, 5])
 				: theme.dropShadow.elevation,
 			zIndex: isCurrentDraggingItem.value ? 1 : 0
 		}
-	}, [draggingItemId.value, isDragging.value])
+	}, [top.value, isCurrentDraggingItem.value, isDraggingDerived.value])
 
 	const gesture = Gesture.Pan()
 		.onStart(() => {
 			isDragging.value = withSpring(1)
 			draggingItemId.value = item.index
+			currentIndex.value =
+				currentItemPositionsDerived.value[item.index].updatedIndex
+		})
+		.onUpdate(({translationY}) => {
+			if (draggingItemIdDerived.value === null) {
+				return
+			}
+
+			const newTop =
+				currentItemPositionsDerived.value[draggingItemIdDerived.value]
+					.updatedTop + translationY
+
+			if (newTop < MIN_TOP || newTop > maxTop) {
+				return
+			}
+
+			top.value = newTop
+			newIndex.value = Math.floor(
+				(newTop + LIST_ITEM_HEIGHT_WITH_MARGIN / 2) /
+					LIST_ITEM_HEIGHT_WITH_MARGIN
+			)
+
+			if (newIndex.value !== currentIndex.value) {
+				const newIndexItemKey = getKeyOfValue(
+					newIndex.value,
+					currentItemPositionsDerived.value
+				)
+				const currentIndexItemKey = getKeyOfValue(
+					currentIndex.value,
+					currentItemPositionsDerived.value
+				)
+
+				if (newIndexItemKey !== null && currentIndexItemKey !== null) {
+					currentItemPositions.value = {
+						...currentItemPositionsDerived.value,
+						[newIndexItemKey]: {
+							...currentItemPositionsDerived.value[
+								newIndexItemKey
+							],
+							updatedIndex: currentIndex.value,
+							updatedTop:
+								currentIndex.value *
+								LIST_ITEM_HEIGHT_WITH_MARGIN
+						},
+						[currentIndexItemKey]: {
+							...currentItemPositionsDerived.value[
+								currentIndexItemKey
+							],
+							updatedIndex: newIndex.value
+						}
+					}
+
+					currentIndex.value = newIndex.value
+				}
+			}
 		})
 		.onEnd(() => {
+			if (currentIndex.value === null || newIndex.value === null) {
+				return
+			}
+
+			top.value = withSpring(
+				newIndex.value * LIST_ITEM_HEIGHT_WITH_MARGIN
+			)
+
+			const currentDragIndexItemKey = getKeyOfValue(
+				currentIndex.value,
+				currentItemPositionsDerived.value
+			)
+
+			if (currentDragIndexItemKey !== null) {
+				currentItemPositions.value = {
+					...currentItemPositionsDerived.value,
+					[currentDragIndexItemKey]: {
+						...currentItemPositionsDerived.value[
+							currentDragIndexItemKey
+						],
+						updatedTop:
+							newIndex.value * LIST_ITEM_HEIGHT_WITH_MARGIN
+					}
+				}
+			}
 			isDragging.value = withDelay(100, withSpring(0))
 		})
 
+	useAnimatedReaction(
+		() => {
+			return currentItemPositionsDerived.value[item.index].updatedIndex
+		},
+		(currentValue, previousValue) => {
+			if (currentValue !== previousValue) {
+				top.value =
+					currentItemPositionsDerived.value[item.index].updatedIndex *
+					LIST_ITEM_HEIGHT_WITH_MARGIN
+			}
+		}
+	)
+
 	return (
 		<GestureDetector gesture={gesture}>
-			<Animated.View style={[styles.item, animatedStyles]}>
+			<Animated.View style={[styles.itemContainer, animatedStyles]}>
 				{children}
 			</Animated.View>
 		</GestureDetector>
--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,9 +1,9 @@
 import {ReactElement} from 'react'
-
-import ListItem, {LIST_ITEM_HEIGHT} from './item/list-item'
-import {FlatList} from 'react-native-gesture-handler'
 import {ListRenderItemInfo, StyleSheet} from 'react-native'
+import {FlatList} from 'react-native-gesture-handler'
 import {useSharedValue} from 'react-native-reanimated'
+
+import ListItem, {LIST_ITEM_HEIGHT_WITH_MARGIN} from './item/list-item'
 
 type ListProps = {
 	items: any[]
@@ -17,38 +17,40 @@ export type ListItemPositions = {
 	}
 }
 
-export const getInitialListItemPositions = (itemsLength: number) => {
+const getInitialListItemPositions = (items: any[]) => {
 	const initialListItemPositions: ListItemPositions = {}
 
-	for (let i = 0; i < itemsLength; i++) {
+	for (let i = 0; i < items.length; i++) {
 		initialListItemPositions[i] = {
 			updatedIndex: i,
-			updatedTop: i * LIST_ITEM_HEIGHT
+			updatedTop: i * LIST_ITEM_HEIGHT_WITH_MARGIN
 		}
 	}
 
 	return initialListItemPositions
 }
 
-const styles = StyleSheet.create({
-	contentContainer: {
-		alignItems: 'center'
-	}
-})
-
 const List = ({items, renderItemContent}: ListProps) => {
 	const currentItemPositions = useSharedValue<ListItemPositions>(
-		getInitialListItemPositions(items.length)
+		getInitialListItemPositions(items)
 	)
 	const isDragging = useSharedValue<0 | 1>(0)
 	const draggingItemId = useSharedValue(-1)
+
+	const styles = StyleSheet.create({
+		listContainer: {
+			width: '100%',
+			paddingHorizontal: 32,
+			height: LIST_ITEM_HEIGHT_WITH_MARGIN * items.length
+		}
+	})
 
 	return (
 		<FlatList
 			// This prop is necessary to be able to tap the save icon for the list item input,
 			// since tapping outside the input ignores other click events by default.
 			removeClippedSubviews={false}
-			contentContainerStyle={styles.contentContainer}
+			contentContainerStyle={styles.listContainer}
 			data={items}
 			renderItem={(item) => (
 				<ListItem
@@ -57,6 +59,7 @@ const List = ({items, renderItemContent}: ListProps) => {
 					currentItemPositions={currentItemPositions}
 					isDragging={isDragging}
 					draggingItemId={draggingItemId}
+					maxTop={(items.length - 1) * LIST_ITEM_HEIGHT_WITH_MARGIN}
 				>
 					{renderItemContent(item)}
 				</ListItem>
